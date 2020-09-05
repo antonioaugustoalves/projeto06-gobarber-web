965bbbcff60b57a34c0b375f71dff205540554ad
Antonio A Alves <antonioalves.duque2019@gmail.com>
2020-09-05 17:00:21 -0300

    Erro nos componentes

--- src/pages/SignIn/index.ts
+++ src/pages/SignIn/index.ts
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container, Content, Background } from './styles';
 import { FiLogIn } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 
 const SignIn: React.FC = () => (
